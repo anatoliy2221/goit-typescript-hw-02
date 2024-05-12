d72b08b2ebbb2662bbf76cfe7f8fc507c0a87056
Anatoliy <anatoliy2221@gmail.com>
2024-05-12 23:14:05 +0300

    add2

--- src/components/loadMoreBtn/LoadMoreBtn.tsx
+++ src/components/loadMoreBtn/LoadMoreBtn.tsx
@@ -1,6 +1,13 @@
+import { FC } from "react";
 import css from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ children, onClick, disabled }) => {
+interface loadMoreBtnProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  disabled: boolean;
+}
+
+const LoadMoreBtn: FC<loadMoreBtnProps> = ({ children, onClick, disabled }) => {
   return (
     <button className={css.button} onClick={onClick} disabled={disabled}>
       {children}
--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => {
       visible={true}
       height="80"
       width="80"
-      color="#4fa94d"
+      // color="#4fa94d"
       ariaLabel="progress-bar-loading"
       wrapperStyle={{}}
       wrapperClass=""
